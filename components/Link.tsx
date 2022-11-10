3257138b586eaf616cbc851810d5416c48b31f98
vul <mohamedsali602@live.com>
2022-11-10 14:53:10 +0100

    this was causing probably a hydration issue

--- components/Link.tsx
+++ components/Link.tsx
@@ -6,20 +6,7 @@ const CustomLink = ({
   href,
   ...rest
 }: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => {
-  const isInternalLink = href && href.startsWith('/')
-  const isAnchorLink = href && href.startsWith('#')
 
-  if (isInternalLink) {
-    return (
-      <Link href={href}>
-        <a {...rest} />
-      </Link>
-    )
-  }
-
-  if (isAnchorLink) {
-    return <a href={href} {...rest} />
-  }
 
   return <a target="_blank" rel="noopener noreferrer" href={href} {...rest} />
 }
