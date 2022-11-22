8a9df02611e005af35c65e93c2a5d71a8ffdfe56
vuture990 <mohamedsali602@live.com>
2022-11-22 03:03:50 +0100

    static

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,7 +10,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/vulture990/portofolio3.0',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/me.jpeg',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: 'static/images/twitter-card.png',
   email: 'mohamedsali602@live.com',
   github: 'https://github.com/vulture990',
   twitter: 'https://twitter.com/mohamedsalbi',
